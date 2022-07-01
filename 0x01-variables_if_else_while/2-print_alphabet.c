946e63b4959c8ea815afb29d740dc3208f9fc144
Caleb Arodu <callyx2002@gmail.com>
2022-07-01 16:16:20 -0700

    lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * 
+ *
  * Return: Always 0 (Success)
  *
  */
@@ -10,8 +10,8 @@ int main(void)
 	char letter;
 	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
-		
+
 	putchar('\n');
-	
+
 	return (0);
 }
