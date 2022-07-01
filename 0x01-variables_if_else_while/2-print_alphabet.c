faff687247b42a43ceeac3a6af6d78b87d94f389
Caleb Arodu <callyx2002@gmail.com>
2022-07-01 15:00:30 -0700

    lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * 
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void) 
+{
+	char c;
+	for (c = 'a'; a <= 'z'; a++)
+	{
+		putchar(c);
+		putchar('\n');
+	}
+	return (0);
+}
