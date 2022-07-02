4fac803d8ea86b279ffcf99708e0dec90d6843d0
Caleb Arodu <callyx2002@gmail.com>
2022-07-01 19:25:59 -0700

    alx

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main -Wntrh point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int n, x;
+	n = 48;
+	x = 48;
+
+	while (x < 58)
+	{
+		n = 48;
+		while (n < 58)
+		{
+			if (x != n && x < n)
+			{
+				putchar(x);
+				putchar(n);
+				if (n == 57 && x == 56)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			n++
+		}
+		x++
+	}
+	putchar('\n');
+	return (0);
+}
