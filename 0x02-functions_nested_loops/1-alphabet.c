1952024aa06ac81767ebce7c39a58117ee7910b6
Caleb Arodu <callyx2002@gmail.com>
2022-07-05 15:14:54 -0700

    lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *print_alphabet => prints letters of the alphabet in lower case
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	int i:
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+	_putchar('\n');
+
+	return (0);
+}
