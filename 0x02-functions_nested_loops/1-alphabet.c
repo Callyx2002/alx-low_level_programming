b7886f91d603beb3a9bb1c6edf68390b676cab51
Caleb Arodu <callyx2002@gmail.com>
2022-07-05 15:25:20 -0700

    nested lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * main - Entry point
- *print_alphabet => prints letters of the alphabet in lower case
+ *print_alphabet - function prints letters of the alphabet in lower case
  *
  * Return: Always 0
  */
