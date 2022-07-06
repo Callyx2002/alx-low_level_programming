54b460c7a11b4d5f6aac8b51474fc06cffe29ba4
Caleb Arodu <callyx2002@gmail.com>
2022-07-06 03:44:13 -0700

    4-isaplha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * _isalpha - tells us if the sunction is an upper or a lowet case
- * @c: is a character
+ * _isalpha - tells us if the sunction is an upper or a lower case
+ * @c: is a character to be checked on
  * Return: returns 1 or 0 based on the condition
  */
 int _isalpha(int c)
