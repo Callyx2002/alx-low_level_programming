a3c30ee473490d8b50d145bd3cf993ff728d9559
Caleb Arodu <callyx2002@gmail.com>
2022-07-06 16:46:51 -0700

    fixed

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,17 +1,9 @@
 #include "main.h"
-
 #include <stdio.h>
-
-
-
 /**
- *
- *  * print_to_98 - check the code
- *
- *   *@n: the integer value it receives
- *
- *    * Return: Always 0.
- *
+ * print_to_98 - check the code
+ * @n: the integer value it receives
+ * Return: Always 0.
  */
 void print_to_98(int n)
 {
