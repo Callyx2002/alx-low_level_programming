dd3f226ab91970594f3cd8156759c3ce95d10027
Caleb Arodu <callyx2002@gmail.com>
2022-07-06 16:30:26 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - checks the code
+ * @a - an integer
+ * @b - an integer
+ * Return: returns a value based on the condition
+ */
+int add(int a, int b)
+{
+	int c = a + b;
+
+	return (c);
+}
