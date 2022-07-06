90b219ae7fd014220c99b4d3d91d264d34e9f327
Caleb Arodu <callyx2002@gmail.com>
2022-07-06 04:32:49 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,18 +2,23 @@
 /**
  * print_sign - prints the sign of a number
  * @n: character
- * Return: returns 0 and 1 based on the conditions
+ * Return: returns 0, -1 and 1 based on the conditions
  */
 int print_sign(int n)
 {
-	if (n > 1)
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
+	else if (n = 0)
+	{
+		_putchar('0');
+		return (0);
+	}
 	else
 	{
 		_putchar('-');
-		return (0);
+		return ('-1');
 	}
 }
