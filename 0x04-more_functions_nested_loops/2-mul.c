6b9c4d26135f5f2d57be2dba11cd07e473c3da16
Caleb Arodu <callyx2002@gmail.com>
2022-07-08 03:18:39 -0700

    late submission!!

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multiplication
+ * @a: integer
+ * @b: integer
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - checks for the numbers
+ * Return: no return
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
