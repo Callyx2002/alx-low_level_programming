238a8ac3d4f853f87feb0eef9106620a25dfe954
Callyx2002 <107306629+Callyx2002@users.noreply.github.com>
2022-07-23 11:13:58 +0100

    Create 3-islower.c

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _islower - checks if the character is lower case or not
+ * @c: is a character
+ * Return: prints 1 or 0 depending on the condition
+ *
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
