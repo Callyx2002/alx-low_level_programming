e4fa2bdcc8ec43bdd5113a3db65070cc8925507b
Caleb Arodu <callyx2002@gmail.com>
2022-07-26 02:42:29 -0700

    malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -5,7 +5,6 @@
  * _strdup - Returns a pointer to a newly-allocated space in memory
  *           containing a copy of the string given as parameter.
  * @str: The string to be copied.
- *
  * Return: If str == NULL or insufficient memory is available - NULL.
  *         Otherwise - a pointer to the duplicated string.
  */
--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -6,7 +6,6 @@
  *               integers with each element initalized to 0.
  * @width: The width of the 2-dimensional array.
  * @height: The height of the 2-dimensional array.
- *
  * Return: If width <= 0, height <= 0, or the function fails - NULL.
  *         Otherwise - a pointer to the 2-dimensional array of integers.
  */
