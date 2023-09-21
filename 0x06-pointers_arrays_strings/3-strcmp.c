ed756cd387d4b97e24b1f80d7a7e9ff7dc2635f2
Caleb Arodu <calebesst01@gmail.com>
2023-09-21 02:30:15 +0100

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: difference
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return ((s1[0] + '0') - (s2[0] +'0'));
+}
