58be8906bf88d1b01f9eafbc94395d940bf08412
Caleb Arodu <calebesst01@gmail.com>
2023-10-03 00:44:24 +0100

    done

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: arg count
+ * @argv: arg vector
+ * Return: 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
